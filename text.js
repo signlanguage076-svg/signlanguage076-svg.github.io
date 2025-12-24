dbbcf3f1e2eca6a16b05d37491272e2a57160cce
signlanguage076-svg <signlanguage076@gmail.com>
2025-12-24 11:52:57 +0900

    Update text.js

--- text.js
+++ text.js
@@ -10,16 +10,6 @@
   
   // Video
   let video;
-  let flippedVideo;
-  // To store the classification
-  let label = "";
-
-  // Load the model first
-  function preload() {
-    classifier = ml5.imageClassifier(imageModelURL + 'model.json');
-  }
-
- let video;
 function setup() {
   createCanvas(400, 400);
 
@@ -41,6 +31,15 @@ function setup() {
 function draw() {
   image(video, 0, 0);
 }
+
+  let flippedVideo;
+  // To store the classification
+  let label = "";
+
+  // Load the model first
+  function preload() {
+    classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+  }
 
   function draw() {
     background(0);
