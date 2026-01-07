aa2fd5ab7be685ee1b4bc4e6e5ac2738f107327b
signlanguage076-svg <signlanguage076@gmail.com>
2026-01-07 10:53:07 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,17 +19,23 @@
     classifier = ml5.imageClassifier(imageModelURL + 'model.json');
   }
 
-  function setup() {
-    createCanvas(320, 260);
-    // Create the video
-    video = createCapture(VIDEO);
-    video.size(320, 240);
-    video.hide();
+let capture;
 
-    flippedVideo = ml5.flipImage(video);
-    // Start classifying
-    classifyVideo();
-  }
+  function setup() {
+    createCanvas(windowWidth, windowHeight);// キャンバスを作成
+   // カメラオプションを設定
+  const constraints = {
+    video: {
+       facingMode: { exact: "environment" }
+    }
+  };
+
+  // createCaptureにオプションを渡す
+  capture = createCapture(constraints);
+  capture.size(640, 480);
+  // capture要素はデフォルトでDOMに追加されるので、非表示にする
+  capture.hide();
+}
 
   function draw() {
     background(0);
