55a7c5c1c0c7a04c9172c85fef90ab9ac823afa0
signlanguage076-svg <signlanguage076@gmail.com>
2026-01-07 10:25:58 +0900

    Update sketch.js

--- p5.js
+++ p5.js
@@ -3,59 +3,65 @@
 <script src="https://cdn.jsdelivr.net/npm/p5@latest/lib/addons/p5.dom.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/ml5@latest/dist/ml5.min.js"></script>
 <script type="text/javascript">
-  // Classifier Variable
-  let classifier;
-  // Model URL
-  let imageModelURL = 'https://teachablemachine.withgoogle.com/models/9lyEEnrtg/';
-  
-  // Video
-  let video;
-  let flippedVideo;
-  // To store the classification
-  let label = "読み込み中・・・";
-
-  // Load the model first
-  function preload() {
-    classifier = ml5.imageClassifier(imageModelURL + 'model.json');
-  }
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-
-  video = createCapture({
-    video: {
-      facingMode: { exact: "environment" }
-    },
-    audio: false
-  });
-  video.size(640, 480);
-  video.hide();
-  classifyVideo();
-}
-
-function draw() {
-  background(0);
-
-  if (video) {
-    image(flippedVideo, 0, 0, width, height);
-  }
-
-  // 「手話してない」時は表示しない
-  if (label !== "none" && confidence > 0.7) {
-    drawFloatingText(label);
-  }
-}
-
-function classifyVideo() {
-  classifier.classify(video, gotResult);
-}
-
-function gotResult(error, results) {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  label = results[0].label; // ← 認識結果（あ / い / う / 何もしてない）
-  classifyVideo();
-}
+  // Classifier Variable
+  let classifier;
+  // Model URL
+  let imageModelURL = 'https://teachablemachine.withgoogle.com/models/9lyEEnrtg/';
+  
+  // Video
+  let video;
+  let flippedVideo;
+  // To store the classification
+  let label = "";
+ 
+  // Load the model first
+  function preload() {
+    classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+  }
+ 
+  function setup() {
+    createCanvas(320, 260);
+    // Create the video
+    video = createCapture(VIDEO);
+    video.size(320, 240);
+    video.hide();
+ 
+    flippedVideo = ml5.flipImage(video);
+    // Start classifying
+    classifyVideo();
+  }
+ 
+  function draw() {
+    background(0);
+    // Draw the video
+    image(flippedVideo, 0, 0);
+ 
+    // Draw the label
+    fill(255);
+    textSize(16);
+    textAlign(CENTER);
+    text(label, width / 2, height - 4);
+  }
+ 
+  // Get a prediction for the current video frame
+  function classifyVideo() {
+    flippedVideo = ml5.flipImage(video)
+    classifier.classify(flippedVideo, gotResult);
+    flippedVideo.remove();
+ 
+  }
+ 
+  // When we get a result
+  function gotResult(error, results) {
+    // If there is an error
+    if (error) {
+      console.error(error);
+      return;
+    }
+    // The results are in an array ordered by confidence.
+    // console.log(results[0]);
+    label = results[0].label;
+    // Classifiy again!
+    classifyVideo();
+  }
 </script>
