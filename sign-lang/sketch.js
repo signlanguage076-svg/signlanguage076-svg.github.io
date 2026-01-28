8533a71ed0ecc732e82882755248ecf096a54f4f
signlanguage076-svg <signlanguage076@gmail.com>
2026-01-28 13:10:29 +0900

    Create sketch.js

---
+++ sign-lang/sketch.js
@@ -0,0 +1,45 @@
+const URL = "https://teachablemachine.withgoogle.com/models/QG21aQqcP/";
+
+let model, webcam, labelContainer, maxPredictions;
+
+async function startCamera() {
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
+
+  model = await tmImage.load(modelURL, metadataURL);
+  maxPredictions = model.getTotalClasses();
+
+  const flip = false;
+  webcam = new tmImage.Webcam(480, 640, flip);
+
+  await webcam.setup({ facingMode: { exact: "environment" }});
+  
+  await webcam.play();
+  window.requestAnimationFrame(loop);
+
+  document.getElementById("webcam-container").appendChild(webcam.canvas);
+  labelContainer = document.getElementById("label-container");
+}
+
+async function loop() {
+  webcam.update();
+  await predict();
+  window.requestAnimationFrame(loop);
+}
+
+async function predict() {
+  const prediction = await model.predict(webcam.canvas);
+
+let best = prediction[0];
+for (let i = 1; i < prediction.length; i++) {
+  if (prediction[i].probability > best.probability) {
+    best = prediction[i];
+  }
+};
+
+  if (best.probability >= 0.5) {
+  labelContainer.innerText = best.className;
+　} else {
+    labelContainer.innerHTML = "";
+ }
+}
